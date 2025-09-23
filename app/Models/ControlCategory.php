f1db2c7b5a32a304abad6f541fdd7b2b751ffab4
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-09-23 23:28:40 +0300

    control category model

--- app/Models/ControlCategory.php
+++ app/Models/ControlCategory.php
@@ -1,10 +1,24 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class ControlCategory extends Model {
-  use HasFactory;
-  protected $fillable = ['name','description'];
-  public function controls(){ return $this->hasMany(Control::class,'category_id'); }
+class ControlCategory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    /**
+     * Relationships
+     */
+    public function controls()
+    {
+        return $this->hasMany(Control::class, 'category_id');
+    }
 }
