7d801db1942ca5b310f01b6236df92aeba3cf342
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-09-23 23:21:36 +0300

    updates control mapping controller

--- app/Http/Controllers/API/ControlMappingController.php
+++ app/Http/Controllers/API/ControlMappingController.php
@@ -1,20 +1,31 @@
 <?php
+
 namespace App\Http\Controllers\API;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Control;
 
 class ControlMappingController extends Controller
 {
-    public function mapRisks(Request $r, Control $control){
+    public function mapRisks(Request $r, Control $control)
+    {
         $data = $r->validate([
-            'risk_ids'=>'required|array',
-            'risk_ids.*'=>'exists:risks,id',
+            'risk_ids'   => 'required|array',
+            'risk_ids.*' => 'exists:risks,id',
         ]);
+
         $control->risks()->syncWithoutDetaching($data['risk_ids']);
-        return $control->risks()->withPivot(['effectiveness_rating','residual_impact'])->get();
+
+        return $control->risks()
+            ->withPivot(['effectiveness_rating', 'residual_impact'])
+            ->get();
     }
-    public function risks(Control $control){
-        return $control->risks()->with(['category','owner'])->get();
+
+    public function risks(Control $control)
+    {
+        return $control->risks()
+            ->with(['category', 'owner'])
+            ->get();
     }
 }
