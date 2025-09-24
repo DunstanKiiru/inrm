7b5450851b1286e269f5560829ea5bee437da3e2
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-09-24 11:47:19 +0300

    adds risk appetite profile model

--- app/Models/RiskAppetiteProfile.php
+++ app/Models/RiskAppetiteProfile.php
@@ -1,15 +1,20 @@
 <?php
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class RiskAppetiteProfile extends Model {
-  use HasFactory;
-  protected $fillable = [
-    'name', 'description'
-  ];
+class RiskAppetiteProfile extends Model
+{
+    use HasFactory;
 
-  public function thresholds() {
-    return $this->hasMany(RiskThreshold::class, 'profile_id');
-  }
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function thresholds()
+    {
+        return $this->hasMany(RiskThreshold::class, 'profile_id');
+    }
 }
