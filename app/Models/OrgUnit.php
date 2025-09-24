e1c39483162b14f3d885b9f4d423d02eed49a674
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-09-24 11:46:19 +0300

    updates org unit model

--- app/Models/OrgUnit.php
+++ app/Models/OrgUnit.php
@@ -1,23 +1,30 @@
 <?php
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class OrgUnit extends Model {
-  use HasFactory;
-  protected $fillable = [
-    'name', 'parent_id'
-  ];
+class OrgUnit extends Model
+{
+    use HasFactory;
 
-  public function parent() {
-    return $this->belongsTo(OrgUnit::class, 'parent_id');
-  }
+    protected $fillable = [
+        'name',
+        'parent_id',
+    ];
 
-  public function children() {
-    return $this->hasMany(OrgUnit::class, 'parent_id');
-  }
+    public function parent()
+    {
+        return $this->belongsTo(OrgUnit::class, 'parent_id');
+    }
 
-  public function risks() {
-    return $this->hasMany(Risk::class, 'org_unit_id');
-  }
+    public function children()
+    {
+        return $this->hasMany(OrgUnit::class, 'parent_id');
+    }
+
+    public function risks()
+    {
+        return $this->hasMany(Risk::class, 'org_unit_id');
+    }
 }
